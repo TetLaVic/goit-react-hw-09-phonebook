85326c03f698ff1388bbd3742a4707c276961e4c
Tetiana Lavrenko <tet.lavrenko@gmail.com>
2021-05-05 19:21:47 +0200

    refactored RegisterPage with hooks

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-// import AuthProvider from './components/context/AuthProvider';
+import AuthProvider from './components/context/AuthProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store.store}>
         <PersistGate loading={null} persistor={store.persistor}>
-          {/* <AuthProvider> */}
-          <App />
-          {/* </AuthProvider> */}
+          <AuthProvider>
+            <App />
+          </AuthProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
--- src/pages/Register/RegisterPage.js
+++ src/pages/Register/RegisterPage.js
@@ -1,74 +1,91 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, connect } from 'react-redux';
 import { v4 as genId } from 'uuid';
 import authOperations from '../../redux/auth/auth-operations';
 
-class RegisterPage extends Component {
-  state = { name: '', email: '', password: '' };
+function RegisterPage({ onRegister }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  reset = () => {
-    this.setState({ name: '', email: '', password: '' });
+  const reset = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
   };
 
-  handleChange = event => {
+  // const dispatch = useDispatch();
+
+  // const onRegister = () => dispatch(authOperations.register);
+  const handleChange = event => {
     event.preventDefault();
     const { name, value } = event.currentTarget;
-    const currentState = this.state;
-    const updateState = { [name]: value };
-    this.setState({ ...currentState, ...updateState });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'email':
+        setEmail(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        console.warn(`Field with a type ${name} cannot be processed.`);
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onRegister(this.state);
-    this.reset();
+    onRegister({ name, email, password });
+    reset();
   };
 
-  render() {
-    // const { name, email, password } = this.state;
-    const nameInputId = genId();
-    const emailInputId = genId();
-    const passwordInputId = genId();
-    return (
-      <>
-        <h2>Registration page</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor={nameInputId}>
-            Name
-            <input
-              type="text"
-              name="name"
-              id={nameInputId}
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor={emailInputId}>
-            Email
-            <input
-              type="email"
-              name="email"
-              id={emailInputId}
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
+  const nameInputId = genId();
+  const emailInputId = genId();
+  const passwordInputId = genId();
+  return (
+    <>
+      <h2>Registration page</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={nameInputId}>
+          Name
+          <input
+            type="text"
+            name="name"
+            id={nameInputId}
+            value={name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor={emailInputId}>
+          Email
+          <input
+            type="email"
+            name="email"
+            id={emailInputId}
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
 
-          <label htmlFor={passwordInputId}>
-            Password
-            <input
-              type="password"
-              name="password"
-              id={passwordInputId}
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Register</button>
-        </form>
-      </>
-    );
-  }
+        <label htmlFor={passwordInputId}>
+          Password
+          <input
+            type="password"
+            name="password"
+            id={passwordInputId}
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 }
 
 const mapDispatchToProps = {
